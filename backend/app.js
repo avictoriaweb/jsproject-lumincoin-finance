f57a2f88625962922dd06ef7afd4d5e20e515b2e
avictoriaweb <victoria.albul@gmail.com>
2026-03-10 10:50:44 +0300

    Update app.js for render

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,13 @@ const balanceRoutes = require('./routes/balance.routes');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  // Замени на свою ссылку от Netlify
+  origin: 'https://singular-buttercream-86095b.netlify.app', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use("/api", authRoutes);
 app.use("/api/categories/expense", expenseCategoriesRoutes);
